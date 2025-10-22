52f7500cc20cd00b4a4e194994aea4385acd6833
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2025-10-22 23:23:04 +0700

    update

---
+++ bt-react7/src/components/UsersList.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function UsersList() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios
+
+    }, []);
+}
+
+export default UsersList;
